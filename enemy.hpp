05d81bc7713c0a30a58b0dcb948ef9040836147f
markd315 <markd315@gmail.com>
2016-04-09 03:24:58 -0400

    Update enemy.hpp
    Added some chase math.  Check it for me when we instantiate some players and enemies.

--- enemy.hpp
+++ enemy.hpp
@@ -1,8 +1,8 @@
 #ifndef ENEMY_H
 #define ENEMY_H
-
+#include <math.h>
 #include <sprite.h>
-
+#define PI 3.14159265
 
 class Enemy : public sprite
 {
@@ -27,6 +27,41 @@ class Enemy : public sprite
         void Set_moveSequence(char *sequence) {
             aiMoveSequence = sequence;
         }
+        void Set_player(Player player){
+        	toChase = player;
+        }
+        void stepForward(){
+        	if(direction==45)
+        		direction+=(1.0-((double)rand(1))*2.0);
+        	if(direction==135)
+        		direction+=(1.0-((double)rand(1))*2.0);
+        	if(direction==225)
+        		direction+=(1.0-((double)rand(1))*2.0);
+        	if(direction==315)
+        		direction+=(1.0-((double)rand(1))*2.0); //Just some stupid tiebreaker code for 45 degree angle chases.
+        	direction%=360;
+        	if(direction<=45 || direction > 315)
+        		xPosition++;
+        	else if(direction<=135)
+        		yPosition--;
+        	else if(direction<=225)
+        		xPosition--;
+        	else if(direction<=315)
+        		yPosition++;
+        }
+        double toPlayer(){//degrees
+        //if y is negative and x isn't +270, both neg add 180, y pos x neg add 90.
+        double x= toChase.xPosition - xPosition;
+        double y= yPosition - toChase.yPosition;
+        double r = (x*x + y*y)^(1.0/2.0);
+   	double theta = acos(x/r)*180.0/PI;
+        if(y>=0 && x<0) //Uh yeah someone check my work on this logic LOL
+        	theta+=90.0;
+        if(y<0 && x<0)
+        	theta+=180.0;
+        if(y<0 && x>=0)
+        	theta+=270.0;
+        }
         void move(){
         char curr = aiMoveSequence[moveIndex];
         switch(curr){
@@ -34,6 +69,11 @@ class Enemy : public sprite
           case 'A':
           case 'a':
           attack(); //TODO implement void method that deals damage to other entities.
+          break;
+          case 'C':
+          case 'c':
+          Set_direction(toPlayer());
+          stepForward();
           break;
           case 'U':
           case 'u':
@@ -69,6 +109,7 @@ class Enemy : public sprite
         float damageOnContact;
         char *aiMoveSequence;
         int moveIndex;
+        Player toChase;
 };
 
 #endif // PLAYER_H
