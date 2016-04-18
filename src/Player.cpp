820af75ddf43e12c9d37b0907c44e22801de787b
f0lie <alexdiep2501@gmail.com>
2016-04-18 16:55:52 -0400

    formating

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,13 +1,16 @@
 #include "include/Player.h"
 
 Player::Player(bool isSolid, int xPosition, int yPosition, double dir, std::string fileName)
-        : Entity(isSolid, xPosition, yPosition, dir, fileName)    // Call the superclass constructor in the subclass' initialization list.
-        {
-            m_weaponDamage =1;
-            m_weaponRange =1;
-            m_floor=1;
-            m_score=0;
-        }
-void Player::attack(){
-	//implement.  Play a sound?
+        : Entity(isSolid, xPosition, yPosition, dir,
+                 fileName)    // Call the superclass constructor in the subclass' initialization list.
+{
+    m_weaponDamage = 1;
+    m_weaponRange = 1;
+    m_floor = 1;
+    m_score = 0;
+}
+
+void Player::attack()
+{
+    //implement.  Play a sound?
 }
